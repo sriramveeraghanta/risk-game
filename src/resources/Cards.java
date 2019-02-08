f3785a2ada5facc3d6d8f37b051e7a93a9753ba9
HarishJayasankar <46975286+HarishJayasankar@users.noreply.github.com>
2019-02-08 06:01:26 +0530

    Adding new Class files for Dice,Cards,Deck

---
+++ src/resources/Cards.java
@@ -0,0 +1,53 @@
+package resources;
+
+import common.EnumClass.UnitType;
+/**
+ * 
+ * This is an entity class for Cards
+ * @author Harish Jayasankar
+ *
+ */
+
+public class Cards {
+	/**
+	 * The @cardType .
+	 */
+	private UnitType cardType;
+
+	/**
+	 * The @territory .
+	 */
+	private Country territory;
+
+     /**
+	 * @return the cardType
+	 */
+	public UnitType getCardType() {
+		return cardType;
+	}
+
+	/**
+	 * @param cardType the cardType to set
+	 */
+	public Cards(UnitType cardType) {
+		this.cardType = cardType;
+	}
+
+	/**
+	 * @return the territory
+	 */
+	public Country getTerritory() {
+		return territory;
+	}
+
+	/**
+	 * @param territory the territory to set
+	 */
+	public void setTerritory(Country territory) {
+		this.territory = territory;
+	}
+
+
+	
+	
+}
