c95bbe557094b5b1067284e2a52a30fd1a639eac
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2019-03-02 11:55:27 -0500

    delete launcher and moved code to game.java

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,24 @@
+import java.awt.EventQueue;
+
+import controllers.GameController;
 
 public class Game {
-
+	/*
+	 * This is the main method.
+	 * */
+	public static void main(String args[]) {
+		// Logs
+		System.out.println("Start Game");
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameController controller = new GameController();
+					controller.showHomeView();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		System.out.println("end");
+	}
 }
