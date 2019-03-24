18f41e93461880026e3692723cb25e93cae9fe1b
eliedosh <12969695+eliedosh@users.noreply.github.com>
2019-03-24 13:13:40 -0400

    fixed reinforcement card bug (#65)

--- src/main/controllers/AttackPhaseDialogController.java
+++ src/main/controllers/AttackPhaseDialogController.java
@@ -1,9 +1,11 @@
 package main.controllers;
 
 import main.models.GameModel;
+import main.models.PlayerModel;
 
 public class AttackPhaseDialogController {
     private GameModel gameModel;
+    private PlayerModel currentPlayer;
 
     public void attackAction() {
 
@@ -13,7 +15,12 @@ public class AttackPhaseDialogController {
         return gameModel;
     }
 
-    public void setGameModel(GameModel gameModel) {
+    public PlayerModel getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void setGameModel(GameModel gameModel, PlayerModel currentPlayer) {
         this.gameModel = gameModel;
+        this.currentPlayer = currentPlayer;
     }
 }
