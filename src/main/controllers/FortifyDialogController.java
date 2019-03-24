f1bfbc43c3d7bd5ffcccfae9d98afcb99845b4e4
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2019-03-24 10:48:34 -0400

    phase dailog boxes and ui controllers implementation (#63)

---
+++ src/main/controllers/FortifyDialogController.java
@@ -0,0 +1,18 @@
+package main.controllers;
+
+import main.models.GameModel;
+
+public class FortifyDialogController {
+
+    private GameModel gameModel;
+
+    public  void attackAction() {}
+
+    public GameModel getGameModel() {
+        return gameModel;
+    }
+
+    public void setGameModel(GameModel gameModel) {
+        this.gameModel = gameModel;
+    }
+}
---
+++ src/main/controllers/ReinforcementDialogController.java
@@ -0,0 +1,22 @@
+package main.controllers;
+
+import main.models.GameModel;
+
+public class ReinforcementDialogController {
+
+    private GameModel gameModel;
+
+    public void addArmyAction() {
+    }
+
+    public void swapCardAction() {
+    }
+
+    public GameModel getGameModel() {
+        return gameModel;
+    }
+
+    public void setGameModel(GameModel gameModel) {
+        this.gameModel = gameModel;
+    }
+}
