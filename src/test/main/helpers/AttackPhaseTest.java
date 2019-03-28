be1a14f5554b886b08eea022320630d9c384c8a3
shabnm <shabnamhasan0222@gmail.com>
2019-03-28 14:45:43 -0400

    Final attackPhaseTest having 5 test cases

--- src/test/main/helpers/AttackPhaseTest.java
+++ src/test/main/helpers/AttackPhaseTest.java
@@ -1,24 +1,21 @@
-package test.main.helpers;
+package test.main.helpers; 
 
-import main.helpers.AttackPhase;
-import main.helpers.MapBuilder;
-import main.models.ContinentModel;
-import main.models.CountryModel;
-import main.models.GameModel;
-import main.models.PlayerModel;
-import main.utills.EnumHandler;
-import main.utills.GameCommons;
+import main.models.*;
 import org.junit.Test;
 import org.junit.Before;
-import org.junit.After;
-
+import main.helpers.AttackPhase;
+import main.helpers.MapBuilder;
+import main.utills.EnumHandler;
+import main.utills.GameCommons;
 import java.util.ArrayList;
 
-import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.*;
+import static junit.framework.TestCase.assertEquals;
 
 /**
- * AttackPhase Tester.
- */
+* AttackPhase Tester. 
+*
+*/ 
 public class AttackPhaseTest {
     PlayerModel attackerPlayer;
     PlayerModel defenderPlayer;
@@ -30,192 +27,75 @@ public class AttackPhaseTest {
     ArrayList<CountryModel> countryList;
     ArrayList<PlayerModel> playerList;
     AttackPhase attackPhase;
-
-    @Before
-    public void before() throws Exception {
-        countryList = new ArrayList<>();
-//        playerList = new ArrayList<>();
-//        defenderPlayer = new PlayerModel(EnumHandler.Color.BLACK);
-//        defenderPlayer.setActive(false);
-//        attackerPlayer = new PlayerModel(EnumHandler.Color.BLUE);
-        gameModel = new GameModel();
-        gameCommons = new GameCommons();
-        mapBuilder = new MapBuilder(gameModel);
-        mapBuilder.readMapFile(null);
-        attackerCountry = gameCommons.getCountryModelFromList(gameModel.getCountries(), "Alaska");
-        defenderCountry = gameCommons.getCountryModelFromList(gameModel.getCountries(), "Alberta");
-//        attackerCountry.setNumberOfUnits(5);
-//        defenderCountry.setNumberOfUnits(4);
-        countryList.add(attackerCountry);
-        attackerPlayer.setCountries(countryList);
-        countryList.clear();
-        countryList.add(defenderCountry);
-        defenderPlayer.setCountries(countryList);
-        playerList.add(attackerPlayer);
-        playerList.add(defenderPlayer);
-        gameModel.setPlayers(playerList);
-        attackPhase = new AttackPhase(gameModel, attackerCountry, defenderCountry);
-    }
-
-    @After
-    public void after() throws Exception {
-    }
-
-
     /**
-     * Method: getNumberAttackerDice()
-     * @throws Exception if exception occured it throws the Exception
+     * method which should run before all of the test methods
+     * @throws Exception if exception occur throws Exception
      */
-    @Test
-    public void testGetNumberAttackerDice() throws Exception {
-//TODO: Test goes here... 
-    }
+@Before
+public void before() throws Exception {
+    countryList = new ArrayList<>();
+    playerList = new ArrayList<>();
+    defenderPlayer = new PlayerModel();
+    attackerPlayer = new PlayerModel();
+    defenderPlayer.setColor(EnumHandler.Color.BLACK);
+    defenderPlayer.setActive(false);
+    attackerPlayer.setColor(EnumHandler.Color.BLUE);
+    gameModel = new GameModel();
+    gameCommons = new GameCommons();
+    mapBuilder = new MapBuilder(gameModel);
+    mapBuilder.readMapFile(null);
+    attackerCountry = gameCommons.getCountryModelFromList(gameModel.getCountries(), "Alaska");
+    defenderCountry = gameCommons.getCountryModelFromList(gameModel.getCountries(), "Alberta");
+    attackerCountry.setArmyInCountry(4);
+    defenderCountry.setArmyInCountry(2);
+    countryList.add(attackerCountry);
+    attackerPlayer.setCountries(countryList);
+    countryList.clear();
+    countryList.add(defenderCountry);
+    defenderPlayer.setCountries(countryList);
+    playerList.add(attackerPlayer);
+    playerList.add(defenderPlayer);
+    gameModel.setPlayers(playerList);
+    attackPhase = new AttackPhase(gameModel, attackerCountry, defenderCountry);
+} 
 
+/** 
+* 
+* Method: getNumberofAttackerDice() 
+* 
+*/ 
+@Test
+public void testGetNumberofAttackerDice() throws Exception {
+    assertEquals(3, attackPhase.getNumberOfAttackerDice());
+}
 
+@Test
+public void testGetNumberofAttackerDiceLessThan3() throws Exception {
+    attackerCountry.setArmyInCountry(2);
+    assertEquals(2, attackPhase.getNumberOfAttackerDice());
+}
     /**
-     * Method: setNumberAttackerDice(int numberAttackerDice)
-     * @throws Exception if exception occured it throws the Exception
-     */
-    @Test
-    public void testSetNumberAttackerDice() throws Exception {
-//TODO: Test goes here... 
-    }
+* 
+* Method: getNumberofDefenderDice() 
+* 
+*/ 
+@Test
+public void testGetNumberofDefenderDice() throws Exception {
+    assertEquals(2, attackPhase.getNumberOfDefenderDice());
+}
 
-//    /**
-//     * Method: getCountryAttacker()
-//     */
-//    @Test
-//    public void testGetCountryAttacker() throws Exception {
-////TODO: Test goes here...
-//    }
-
-//    /**
-//     * Method: setCountryAttacker(CountryModel countryAttacker)
-//     */
-//    @Test
-//    public void testSetCountryAttacker() throws Exception {
-////TODO: Test goes here...
-//    }
-
-//    /**
-//     * Method: getCountryDefender()
-//     */
-//    @Test
-//    public void testGetCountryDefender() throws Exception {
-////TODO: Test goes here...
-//    }
-
-//    /**
-//     * Method: setcountryDefender(CountryModel countryDefender)
-//     */
-//    @Test
-//    public void testSetcountryDefender() throws Exception {
-////TODO: Test goes here...
-//    }
-
+@Test
+public void testGetNumberofDefenderDiceLessThan2() throws Exception {
+    defenderCountry.setArmyInCountry(1);
+    assertEquals(1, attackPhase.getNumberOfDefenderDice());
+}
 /**
- *
- * Method: checkIfPlayerCanAttackCountry()
- *
- */
-//@Test
-//public void testCheckIfPlayerCanAttackCountry() throws Exception {
-//    assertTrue(attackPhase.checkIfPlayerCanAttackCountry());
-//}
-
-//    /**
-//     * Method: rollDice()
-//     */
-//    @Test
-//    public void testRollDice() throws Exception {
-////TODO: Test goes here...
-//    }
-
-//    /**
-//     * Method: attackingResult()
-//     */
-//    @Test
-//    public void testAttackingResult() throws Exception {
-////TODO: Test goes here...
-//    }
-
-//    /**
-//     * Method: checkMaxNumberOfDices()
-//     */
-//    @Test
-//    public void testCheckMaxNumberOfDices() throws Exception {
-////TODO: Test goes here...
-//    }
-
-//    /**
-//     * Method: roll(int numberOfDice)
-//     */
-//    @Test
-//    public void testRoll() throws Exception {
-////TODO: Test goes here...
-//    }
-
-//    /**
-//     * Method: assignCardToPlayer(PlayerModel player)
-//     */
-//    @Test
-//    public void testAssignCardToPlayer() throws Exception {
-////TODO: Test goes here...
-//    }
-
-
-//    /**
-//     * Method: assignCountryToWinnerPlayer(PlayerModel winnerPlayer, PlayerModel loserPlayer, String countryName)
-//     */
-//    @Test
-//    public void testAssignCountryToWinnerPlayer() throws Exception {
-////TODO: Test goes here...
-/*
-try { 
-   Method method = AttackPhase.getClass().getMethod("assignCountryToWinnerPlayer", PlayerModel.class, PlayerModel.class, String.class); 
-   method.setAccessible(true); 
-   method.invoke(<Object>, <Parameters>); 
-} catch(NoSuchMethodException e) { 
-} catch(IllegalAccessException e) { 
-} catch(InvocationTargetException e) { 
-} 
-*/
-    //}
-
-//    /**
-//     * Method: assignRemainingCardsToWinnerPlayer(PlayerModel winnerPlayer, PlayerModel loserPlayer)
-//     */
-//    @Test
-//    public void testAssignRemainingCardsToWinnerPlayer() throws Exception {
-////TODO: Test goes here...
-/* 
-try { 
-   Method method = AttackPhase.getClass().getMethod("assignRemainingCardsToWinnerPlayer", PlayerModel.class, PlayerModel.class); 
-   method.setAccessible(true); 
-   method.invoke(<Object>, <Parameters>); 
-} catch(NoSuchMethodException e) { 
-} catch(IllegalAccessException e) { 
-} catch(InvocationTargetException e) { 
-} 
-*/
-//    }
-//
-//    /**
-//     * Method: getDefender(String defenderCountryName)
-//     */
-//    @Test
-//    public void testGetDefender() throws Exception {
-////TODO: Test goes here...
-/* 
-try { 
-   Method method = AttackPhase.getClass().getMethod("getDefender", String.class); 
-   method.setAccessible(true); 
-   method.invoke(<Object>, <Parameters>); 
-} catch(NoSuchMethodException e) { 
-} catch(IllegalAccessException e) { 
-} catch(InvocationTargetException e) { 
-} 
-*/
-  //  }
-
+* 
+* Method: getAllDiceValues(int numberOfDice) 
+* 
+*/ 
+@Test
+public void testGetAllDiceValues() throws Exception {
+    assertNotNull(attackPhase.getAllDiceValues(2));
+}
 } 
