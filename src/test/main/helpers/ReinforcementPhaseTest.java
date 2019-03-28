e7ab75e257ee5dd4f90ebe3176967ce491c1ca8f
shabnm <shabnamhasan0222@gmail.com>
2019-03-28 14:41:56 -0400

    Final reinforcementPhaseTest having 8 test cases

--- src/test/main/helpers/ReinforcementPhaseTest.java
+++ src/test/main/helpers/ReinforcementPhaseTest.java
@@ -1,4 +1,4 @@
-package test.main.helpers; 
+package test.main.helpers;
 
 import main.helpers.MapBuilder;
 import main.helpers.ReinforcementPhase;
@@ -6,17 +6,16 @@ import main.models.*;
 import main.utills.EnumHandler;
 import main.utills.GameCommons;
 import org.junit.Test;
-import org.junit.Before; 
-import org.junit.After;
+import org.junit.Before;
 import static junit.framework.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/** 
-* ReinforcementPhase Tester. 
-*
-*/ 
+/**
+ * ReinforcementPhase Tester.
+ *
+ */
 public class ReinforcementPhaseTest {
     PlayerModel playerModel;
     GameModel gameModel;
@@ -27,40 +26,44 @@ public class ReinforcementPhaseTest {
     CardModel cardModel;
     CardModel cardModel1;
 
-@Before
-public void before() throws Exception {
-    playerModel = new PlayerModel();
-    gameModel = new GameModel();
-    reinforce = new ReinforcementPhase(playerModel, gameModel);
-    mapBuilder = new MapBuilder(gameModel);
-    mapBuilder.readMapFile(null);
-    gameCommons = new GameCommons();
-    cardModel = new CardModel(EnumHandler.CardType.INFANTRY);
-    cardModel.setNumberOfCards(4);
-    cardModel1 = new CardModel(EnumHandler.CardType.INFANTRY);
-    cardModel1.setNumberOfCards(2);
-    country1 = gameCommons.getCountryModelFromList(gameModel.getCountries(),"Venezuala");
-    country2 = gameCommons.getCountryModelFromList(gameModel.getCountries(),"Peru");
-    country3 = gameCommons.getCountryModelFromList(gameModel.getCountries(),"Brazil");
-    country4 = gameCommons.getCountryModelFromList(gameModel.getCountries(),"Argentina");
-} 
-
-@After
-public void after() throws Exception { 
-} 
+    /**
+     * method which should run before all of the test methods
+     * @throws Exception if exception occur throws Exception
+     */
+    @Before
+    public void before() throws Exception {
+        playerModel = new PlayerModel();
+        gameModel = new GameModel();
+        reinforce = new ReinforcementPhase(playerModel, gameModel);
+        mapBuilder = new MapBuilder(gameModel);
+        mapBuilder.readMapFile(null);
+        gameCommons = new GameCommons();
+        cardModel = new CardModel(EnumHandler.CardType.INFANTRY);
+        cardModel.setNumberOfCards(4);
+        cardModel1 = new CardModel(EnumHandler.CardType.INFANTRY);
+        cardModel1.setNumberOfCards(2);
+        country1 = gameCommons.getCountryModelFromList(gameModel.getCountries(),"Venezuala");
+        country2 = gameCommons.getCountryModelFromList(gameModel.getCountries(),"Peru");
+        country3 = gameCommons.getCountryModelFromList(gameModel.getCountries(),"Brazil");
+        country4 = gameCommons.getCountryModelFromList(gameModel.getCountries(),"Argentina");
+    }
 
     /**
      * Method: swapCardsForArmyUnits()
-     * @throws Exception if exception occur it throws Exception
+     * @throws Exception if exception occur throws Exception
      */
     @Test
-public void testSwapCardsForArmyUnitsForArmyGreaterThanZero() throws Exception {
-    ArrayList<CardModel> cardModelList =new ArrayList<>();
-    cardModelList.add(cardModel);
-    playerModel.setDeck(cardModelList);
-    assertEquals(reinforce.swapCardsForArmyUnits(), 5);
-}
+    public void testSwapCardsForArmyUnitsForArmyGreaterThanZero() throws Exception {
+        ArrayList<CardModel> cardModelList =new ArrayList<>();
+        cardModelList.add(cardModel);
+        playerModel.setDeck(cardModelList);
+        assertEquals(reinforce.swapCardsForArmyUnits(), 5);
+    }
 
+    /**
+     * Method: swapCardsForArmyUnits()
+     * @throws Exception if exception occur throws Exception
+     */
     @Test
     public void testSwapCardsForArmyUnitsForArmyEqualZero() throws Exception {
         ArrayList<CardModel> cardModelList =new ArrayList<>();
@@ -69,20 +72,23 @@ public void testSwapCardsForArmyUnitsForArmyGreaterThanZero() throws Exception {
         assertEquals(reinforce.swapCardsForArmyUnits(), 0);
     }
 
+    /**
+     * Method: getArmyUnitsForConqueredContinent()
+     * @throws Exception if exception occur throws Exception
+     */
+    @Test
+    public void testGetArmyUnitsForConqueredContinentForOneContinent() throws Exception {
+        playerModel.addCountry(country1);
+        playerModel.addCountry(country2);
+        playerModel.addCountry(country3);
+        playerModel.addCountry(country4);
+        assertEquals(reinforce.getArmyUnitsForConqueredContinent(),2);
+    }
 
     /**
      * Method: getArmyUnitsForConqueredContinent()
-     * @throws Exception if exception occur it throws Exception
+     * @throws Exception if exception occur throws Exception
      */
-    @Test
-public void testGetArmyUnitsForConqueredContinentForOneContinent() throws Exception {
-    playerModel.addCountry(country1);
-    playerModel.addCountry(country2);
-    playerModel.addCountry(country3);
-    playerModel.addCountry(country4);
-    assertEquals(reinforce.getArmyUnitsForConqueredContinent(),2);
-}
-
     @Test
     public void testGetArmyUnitsForConqueredContinentForSecondContinent() throws Exception {
         playerModel.addCountry(country1);
@@ -96,52 +102,51 @@ public void testGetArmyUnitsForConqueredContinentForOneContinent() throws Except
         assertEquals(reinforce.getArmyUnitsForConqueredContinent(),2);
     }
 
-
     /**
      * Method: getArmyUnitsFromCountries()
-     * @throws Exception if exception occur it throws Exception
+     * @throws Exception if exception occur throws Exception
      */
     @Test
-public void testGetArmyUnitsFromCountries() throws Exception {
-    playerModel.addCountry(country1);
-    playerModel.addCountry(country2);
-    playerModel.addCountry(country3);
-    playerModel.addCountry(country4);
-    assertEquals(reinforce.getArmyUnitsFromCountries(), 3);
-} 
+    public void testGetArmyUnitsFromCountries() throws Exception {
+        playerModel.addCountry(country1);
+        playerModel.addCountry(country2);
+        playerModel.addCountry(country3);
+        playerModel.addCountry(country4);
+        assertEquals(reinforce.getArmyUnitsFromCountries(), 3);
+    }
 
     /**
-     * Method: getNumberOfSimilarCards()
-     * @throws Exception if exception occur it throws Exception
+     *  Method: getNumberOfSimilarCards()
+     * @throws Exception if exception occur throws Exception
      */
     @Test
-public void testGetNumberOfSimilarCards() throws Exception {
-    ArrayList<CardModel> cardModelList =new ArrayList<>();
-    cardModelList.add(cardModel);
-    playerModel.setDeck(cardModelList);
-    assertEquals(reinforce.getNumberOfSimilarCards(), 1);
-}
+    public void testGetNumberOfSimilarCards() throws Exception {
+        ArrayList<CardModel> cardModelList =new ArrayList<>();
+        cardModelList.add(cardModel);
+        playerModel.setDeck(cardModelList);
+        assertEquals(reinforce.getNumberOfSimilarCards(), 1);
+    }
 
     /**
-     *  Method: getNumberOfDifferentCards()
-     * @throws Exception if exception occur it throws Exception
+     * Method: getNumberOfDifferentCards()
+     * @throws Exception if exception occur throws Exception
      */
     @Test
-public void testGetNumberOfDifferentCards() throws Exception {
-    ArrayList<CardModel> cardModelList =new ArrayList<>();
-    cardModelList.add(cardModel);
-    playerModel.setDeck(cardModelList);
-    assertEquals(reinforce.getNumberOfDifferentCards(), 0);
-} 
+    public void testGetNumberOfDifferentCards() throws Exception {
+        ArrayList<CardModel> cardModelList =new ArrayList<>();
+        cardModelList.add(cardModel);
+        playerModel.setDeck(cardModelList);
+        assertEquals(reinforce.getNumberOfDifferentCards(), 0);
+    }
 
     /**
-     * Method: getNumberCardTypeByCardType
-     * @throws Exception if exception occur it throws Exception
+     * Method: getNumberCardTypeByCardType(List<CardModel> cards, EnumHandler.CardType unitType)
+     * @throws Exception if exception occur throws Exception
      */
     @Test
-public void testGetNumberCardTypeByCardType() throws Exception {
-    List<CardModel> card =new ArrayList<>();
-    card.add(cardModel);
-    assertEquals(reinforce.getNumberCardTypeByCardType(card, EnumHandler.CardType.INFANTRY), 4);
-}
+    public void testGetNumberCardTypeByCardType() throws Exception {
+        List<CardModel> card =new ArrayList<>();
+        card.add(cardModel);
+        assertEquals(reinforce.getNumberCardTypeByCardType(card, EnumHandler.CardType.INFANTRY), 4);
+    }
 } 
